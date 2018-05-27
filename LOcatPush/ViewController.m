a5396a8534c6b5f9b2bd84a8bacaced852eddf72
mac <mac@126.com>
2018-05-27 17:16:07 +0800

    commit

--- LOcatPush/ViewController.m
+++ LOcatPush/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LocalPush.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+// 本地推送
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [LocalPush SharePushFromTitle:@"小明对你说：" subtitle:@"你今天中午吃的啥呀啊？" userInfo:@{@"2":@"yyyuuuui"}];
+
+    [LocalPush SharePushFromTitle:@"行程提醒" subtitle:@"下午三点有会，请准时参加！" body:nil userInfo:nil afterDelay:10];
+//    [LocalPush SharePushFromTitle:@"行程提醒" subtitle:@"下午三点有会，请准时参加！" body:nil userInfo:nil afterDelay:20];
+//    [LocalPush SharePushFromTitle:@"行程提醒" subtitle:@"下午三点有会，请准时参加！" body:nil userInfo:nil afterDelay:30];
 }
 
 
